0122c281b5913c910d16ce6941689ef295f80b4a
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-11 22:00:12 +0530

    Update alive.py

--- AtiyaMusic/plugins/alive.py
+++ AtiyaMusic/plugins/alive.py
@@ -10,7 +10,7 @@ from config import BOT_NAME
 async def start(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://te.legra.ph/file/2b1f1ea263f7092937d1f.jpg",
-        caption=f"❤️ ʜᴇʏ {message.from_user.mention}\n\n🔮 ɪ ᴀᴍ {BOT_NAME}\n━━━━━━━━━━━━━━━━━━\n\n✨ ɪ ᴀᴍ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.\n💫 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ ᴊᴏɪɴ ᴏᴜʀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ🤍...\n\n━━━━━━━━━━━━━━━━━━\n❄ ᴍʏ ᴏᴡɴᴇʀ : [˹ᴋʀɪsʜɴᴀ˼](https://t.me/itz_rocks_krishna)\n🌸 ᴀʙᴏᴜᴛ ᴍᴇ  : [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/incorrect_krishna)\n❤‍🔥 ᴍᴇᴇᴛ ᴍᴇ    : [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/frienddd_zoneee\n━━━━━━━━━━━━━━━━━━)",
+        caption=f"❤️ ʜᴇʏ {message.from_user.mention}\n\n🔮 ɪ ᴀᴍ {BOT_NAME}\n━━━━━━━━━━━━━━━━━━\n\n✨ ɪ ᴀᴍ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.\n\n💫 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ ᴊᴏɪɴ ᴏᴜʀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ🤍...\n\n━━━━━━━━━━━━━━━━━━\n❄ ᴍʏ ᴏᴡɴᴇʀ : [˹ᴋʀɪsʜɴᴀ˼](https://t.me/itz_rocks_krishna)\n🌸 ᴀʙᴏᴜᴛ ᴍᴇ  : [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/incorrect_krishna)\n❤‍🔥 ᴍᴇᴇᴛ ᴍᴇ    : [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/frienddd_zoneee)\n━━━━━━━━━━━━━━━━━━",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
