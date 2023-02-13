aad2855ba4b0ba7d4e9c8e420fb6f178f8e3671f
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-13 11:49:30 +0530

    Update start.py

--- AtiyaMusic/utils/inline/start.py
+++ AtiyaMusic/utils/inline/start.py
@@ -27,30 +27,34 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
 
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="⛦ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⛦",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="⛦ ʜᴇʟᴩ ⛦", callback_data="settings_back_helper"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="⛦ sᴜᴩᴩᴏʀᴛ ⛦", url=config.SUPPORT_GROUP
-            ),
-            InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ⛦", user_id=OWNER
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="⛦ sᴏᴜʀᴄᴇ ⛦", callback_data="gib_source"
-            )
-        ],
-     ]
-    return buttons
+
+   buttons = [
+    [
+        InlineKeyboardButton(
+            text="✨ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ✨",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        )
+    ],
+    [
+        InlineKeyboardButton(
+            text="˹sᴜᴘᴘᴏʀᴛ˼", url=config.SUPPORT_GROUP
+        ),
+        InlineKeyboardButton(
+            text="˹ᴜᴘᴅᴀᴛᴇ˼", url=config.SUPPORT_CHANNEL
+        )
+    ],
+    [
+        InlineKeyboardButton(
+            text="♡ ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ ♡", callback_data="settings_back_helper"
+        )
+    ],
+    [
+        InlineKeyboardButton(
+            text="˹sᴏᴜʀᴄᴇ˼", callback_data="gib_source"
+        ),
+        InlineKeyboardButton(
+            text="♥ ᴏᴡɴᴇʀ ♥", user_id=OWNER
+        )
+    ]
+]
+  return buttons
