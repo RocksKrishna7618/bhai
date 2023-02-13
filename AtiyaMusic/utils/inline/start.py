85906579a0d865ab3b2e3324880dc15367f450f9
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-13 19:58:59 +0530

    Update start.py

--- AtiyaMusic/utils/inline/start.py
+++ AtiyaMusic/utils/inline/start.py
@@ -36,7 +36,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="⛦♡ ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ ♡", callback_data="settings_back_helper"
+                text="♡ ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ ♡", callback_data="settings_back_helper"
             )
         ],
         [
